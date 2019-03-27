18dc04d38a267919de66c93609031fd7a4146b4b
Acer <dmitriy.ev.blinov@gmail.com>
2019-03-27 14:53:01 +0700

    Исправление ошибок

--- Arrays/src/Arrays.js
+++ Arrays/src/Arrays.js
@@ -1,27 +1,43 @@
+"use strict";
+
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //Отсортируйте массив по убыванию
-list.sort(sortDesc);
-
-function sortDesc(e1, e2) {
+console.log(list.sort(function (e1, e2) {
     return e2 - e1;
-}
+}));
 
 //Подмассив первых 5 элементов, последних 5 элементов
-var firstFiveElements = list.slice(0, 4);
-var lastFiveElements = list.slice(list.length - 5); //или здесь -4?
+console.log(list.slice(0, 5));
+console.log(list.slice(list.length - 5));
 
 //Найдите сумму четных чисел в массиве
-var sum = 0;
-list.forEach(calculateSum);
-function calculateSum(i) {
-    if (i % 2 === 0) {
-        sum += i;
+var sum = list.reduce (function (sum, element) {
+    if (element % 2 === 0) {
+        return sum + element;
+    } else {
+        return sum;
     }
-}
+});
+
+console.log(sum);
 
 //Создайте массив от 1 до 100 и получите список квадратов четных чисел
 var bigList = [];
-for (var i = 1; i <= 100; i++) {
-    bigList.push(i);
+
+fillList(100, bigList);
+
+function fillList(length, list) {
+    for (var i = 1; i <= length; i++) {
+        list.push(i);
+    }
 }
+
+var powList = bigList.filter(function (item) {
+    return item % 2 === 0;
+});
+powList.forEach(function (el, index, array) {
+    array[index] = Math.pow(el, 2);
+});
+
+console.log(powList);
