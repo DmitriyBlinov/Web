3f9fd66b043c86c77f3ff446b4ced83d1eee930a
Acer <dmitriy.ev.blinov@gmail.com>
2019-03-28 15:59:52 +0700

    Исправление ошибок

---
+++ Objects/src/Objects.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var countries = [
+    {
+        name: "USA",
+        cities: [{name: "New York", population: 8537673},
+            {name: "Los Angeles", population: 3884307},
+            {name: "Chicago", population: 2718782}]
+    },
+    {
+        name: "France",
+        cities: [{name: "Paris", population: 2229621},
+            {name: "Marseille", population: 855393},
+            {name: "Lyon", population: 500715}]
+    },
+    {
+        name: "Russia",
+        cities: [{name: "Moscow", population: 12616000},
+            {name: "Saint Petersburg", population: 5384000},
+            {name: "Novosibirsk", population: 1613000},
+            {name: "Yekaterinburg", population: 1484000}]
+    }
+];
+
+var countryName = countries.find(name => ).name;
