44b6178dae02ee05164503d7304617bfb1fb4f7f
DmitriyBlinov <dmitriy.ev.blinov@gmail.com>
2019-03-30 18:01:09 +0700

    Исправления

--- Arrays/src/Arrays.js
+++ Arrays/src/Arrays.js
@@ -1,17 +1,13 @@
 "use strict";
 
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-//Отсортируйте массив по убыванию
-console.log(list.sort(function (e1, e2) {
+console.log("Сортировка по убыванию: " + list.sort(function (e1, e2) {
     return e2 - e1;
 }));
 
-//Подмассив первых 5 элементов, последних 5 элементов
-console.log(list.slice(0, 5));
-console.log(list.slice(list.length - 5));
+console.log("Первые пять: " + list.slice(0, 5));
+console.log("Последние пять: " + list.slice(list.length - 5));
 
-//Найдите сумму четных чисел в массиве
 var sum = list.reduce(function (sum, element) {
     if (element % 2 === 0) {
         return sum + element;
@@ -19,12 +15,10 @@ var sum = list.reduce(function (sum, element) {
         return sum;
     }
 });
+console.log("Сумма четных: " + sum);
 
-console.log(sum);
 
-//Создайте массив от 1 до 100 и получите список квадратов четных чисел
 var bigList = [];
-
 fillList(100, bigList);
 
 function fillList(length, list) {
@@ -36,8 +30,7 @@ function fillList(length, list) {
 var powList = bigList.filter(function (item) {
     return item % 2 === 0;
 });
-powList.forEach(function (el, index, array) {
-    array[index] = Math.pow(el, 2);
+powList.map(function (value) {
+    return Math.pow(value, 2);
 });
-
-console.log(powList);
+console.log("Квадраты четных: " + powList);
