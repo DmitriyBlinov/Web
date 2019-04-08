10cf9440d16a8ff4611055e0e48e13e43da6f6b0
dmitriy.ev.blinov@gmail.com <Th3yellowl1moncello>
2019-04-08 15:15:22 +0700

    Добавлена функция подсчета кол-ва городов

--- Objects/src/Objects.js
+++ Objects/src/Objects.js
@@ -22,12 +22,30 @@ var countries = [
     }
 ];
 
+var maxCities = findMaxLength(countries, "cities");
+
+function findMaxLength(array, subArray) {
+    var maxLength = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][subArray].length > maxLength) {
+            maxLength = array[i][subArray].length;
+        }
+    }
+    return maxLength;
+}
+
+var maxCitiesCountries = countries.filter(function (item) {
+    return item["cities"].length >= maxCities;
+});
+
+console.log(maxCitiesCountries);
+
 var countriesPopulation = {};
 for (var i = 0; i < countries.length; i++) {
     countriesPopulation[countries[i].name] = calculateSum(countries[i].cities, "population");
 }
 
-function calculateSum (array, key) {
+function calculateSum(array, key) {
     var sum = 0;
     for (var j = 0; j < array.length; j++) {
         sum += array[j][key];
@@ -36,21 +54,3 @@ function calculateSum (array, key) {
 }
 
 console.log(countriesPopulation);
-
-var maxCities = findMaxSubArray(countries, "cities");
-function findMaxSubArray (array, subArray) {
-    var temp = {};
-    var maxLength = 0;
-    for(var i = 0; i < array.length; i++) {
-        if (array[i][subArray].length > maxLength) {
-            maxLength = array[i][subArray].length;
-        }
-    }
-    for(var i = 0; i < array.length; i++) {
-        if (array[i][subArray].length === maxLength) {
-            temp[array[i].name] = maxLength;
-        }
-    }
-    return temp;
-}
-console.log(maxCities);
