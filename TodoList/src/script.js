6aa9346bf1643e9ddee60995d489d3b4def0eff0
dmitriy.ev.blinov@gmail.com <Th3yellowl1moncello>
2019-04-12 14:23:20 +0700

    Бубновая версия редактирования

--- TodoList/src/script.js
+++ TodoList/src/script.js
@@ -12,21 +12,105 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         var li = document.createElement("li");
         li.innerHTML = "<span></span>\
-       <button type='button'>X</button>\
-       \<button type='button'>Edit</button>";
+        <button type='button'>X</button>\
+        \<button type='button'>Edit</button>";
         li.children[0].innerText = text;
         li.children[1].addEventListener("click", function () {
             list.removeChild(li);
         });
         li.children[2].addEventListener("click", function () {
             //TODO редактирование
-            var temp = document.createElement("input");
-            temp.setAttribute("type", "text");
-            temp.setAttribute("value", li.children[0].innerText);
-
-            li.children[0].innerText = "";
-            li.children[0].appendChild(temp);
+            var tempText = li.children[0].innerText;
+            li.innerHTML = "<input type='text' />\
+            <button type='button'>Decline</button>\
+            \<button type='button'>Save</button>";
+            li.children[0].value = tempText;
+            li.children[1].addEventListener("click", function () {
+                li.innerHTML = "<span></span>\
+                <button type='button'>X</button>\
+                \<button type='button'>Edit</button>";
+                li.children[0].innerText = text;
+                li.children[1].addEventListener("click", function () {
+                    list.removeChild(li);
+                });
+                li.children[2].addEventListener("click", function () {
+                    var tempText = li.children[0].innerText;
+                    li.innerHTML = "<input type='text' />\
+                    <button type='button'>Decline</button>\
+                    \<button type='button'>Save</button>";
+                    li.children[0].value = tempText;
+                    li.children[1].addEventListener("click", function () {
+                        li.innerHTML = "<span></span>\
+                        <button type='button'>X</button>\
+                        \<button type='button'>Edit</button>";
+                        li.children[0].innerText = text;
+                        li.children[1].addEventListener("click", function () {
+                            list.removeChild(li);
+                        });
+                    })
+                });
+            });
+            li.children[2].addEventListener("click", function () {
+                var tempText = li.children[0].value;
+                li.innerHTML = "<span></span>\
+                <button type='button'>X</button>\
+                \<button type='button'>Edit</button>";
+                li.children[0].innerText = tempText;
+                li.children[1].addEventListener("click", function () {
+                    list.removeChild(li);
+                });
+                li.children[2].addEventListener("click", function () {
+                    var tempText = li.children[0].innerText;
+                    li.innerHTML = "<input type='text' />\
+                    <button type='button'>Decline</button>\
+                    \<button type='button'>Save</button>";
+                    li.children[0].value = tempText;
+                    li.children[1].addEventListener("click", function () {
+                        li.innerHTML = "<span></span>\
+                        <button type='button'>X</button>\
+                        \<button type='button'>Edit</button>";
+                        li.children[0].innerText = text;
+                        li.children[1].addEventListener("click", function () {
+                            list.removeChild(li);
+                        });
+                    })
+                });
+            })
         });
+
+        function deleteLine() {
+            list.removeChild(li);
+        }
+
+        function edit() {
+            var tempText = li.children[0].innerText;
+            li.innerHTML = "<input type='text' />\
+            <button type='button'>Decline</button>\
+            \<button type='button'>Save</button>";
+            li.children[0].value = tempText;
+            li.children[1].addEventListener("click", decline);
+            li.children[2].addEventListener("click", save);
+        }
+
+        function decline() {
+            var tempText = li.children[0].value;
+            li.innerHTML = "<span></span>\
+                <button type='button'>X</button>\
+                \<button type='button'>Edit</button>";
+            li.children[0].innerText = tempText;
+            li.children[1].addEventListener("click", deleteLine);
+            li.children[2].addEventListener("click", edit);
+        }
+
+        function save() {
+            var tempText = li.children[0].value;
+            li.innerHTML = "<span></span>\
+                <button type='button'>X</button>\
+                \<button type='button'>Edit</button>";
+            li.children[0].innerText = tempText;
+            li.children[1].addEventListener("click", deleteLine);
+            li.children[2].addEventListener("click", edit);
+        }
 
         list.appendChild(li);
         newTextField.value = "";
