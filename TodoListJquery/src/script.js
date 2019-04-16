55f578fc5728df027287b2441bb1305337f3b758
dmitriy.ev.blinov@gmail.com <Th3yellowl1moncello>
2019-04-16 14:15:50 +0700

    Первая часть

---
+++ TodoListJquery/src/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+$(document).ready(function () {
+    var newTextField = $(".new-line");
+    var list = $(".list");
+
+    var createButton = $(".create");
+    createButton.addEventListener("click", function () {
+        var text = newTextField.value;
+        if (text === "") {
+            return;
+        }
+
+        var li = document.createElement("li");
+        createBasicLi(text);
+
+        function deleteLine() {
+            list.removeChild(li);
+        }
+
+        function edit() {
+            var tempText = li.children[0].innerText;
+            li.innerHTML = "<input type='text' />\
+            <button type='button'>Decline</button>\
+            \<button type='button'>Save</button>";
+            li.children[0].value = tempText;
+            li.children[1].addEventListener("click", decline);
+            li.children[2].addEventListener("click", save);
+        }
+
+        function decline() {
+            createBasicLi(text);
+        }
+
+        function save() {
+            var tempText = li.children[0].value;
+            text = li.children[0].value;
+            createBasicLi(tempText);
+        }
+
+        function createBasicLi(text) {
+            li.innerHTML = "<span></span>\
+                <button type='button'>X</button>\
+                \<button type='button'>Edit</button>";
+            li.children[0].innerText = text;
+            li.children[1].addEventListener("click", deleteLine);
+            li.children[2].addEventListener("click", edit);
+        }
+
+        list.appendChild(li);
+        newTextField.value = "";
+    });
+});
